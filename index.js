40651d2a18420121eaa5b9b96b40bc8cbd7ef820
Sara Vieira <hey@iamsaravieira.com>
2018-01-31 11:02:53 +0000

    fix build

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-'use strict';
-const reviews = require('./reviews.json');
+'use strict'
+const reviews = require('./reviews.json')
 
 module.exports = (opts) => {
-	opts = opts || {};
-	let r = reviews;
+  opts = opts || {}
+  let r = reviews
 
-	if (opts.good) {
-		r = reviews.filter(review => review.rating > 5);
-	}
+  if (opts.good) {
+    r = reviews.filter(review => review.rating > 5)
+  }
 
-	if (opts.bad) {
-		r = reviews.filter(review => review.rating < 5);
-	}
+  if (opts.bad) {
+    r = reviews.filter(review => review.rating < 5)
+  }
 
-	return r[Math.floor(Math.random() * r.length)];
-};
+  return r[Math.floor(Math.random() * r.length)]
+}
