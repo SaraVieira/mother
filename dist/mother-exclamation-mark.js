f7e72093f408ab6a8d6117001570cd4e3f9d6ec1
Sara Vieira <hey@iamsaravieira.com>
2018-01-31 10:50:04 +0000

    remove dist

--- dist/mother-exclamation-mark.js
+++
@@ -1,2 +0,0 @@
-var r=require("./reviews.json");module.exports=function(n){var t=r;return(n=n||{}).good&&(t=r.filter(function(r){return r.rating>5})),n.bad&&(t=r.filter(function(r){return r.rating<5})),t[Math.floor(Math.random()*t.length)]};
-//# sourceMappingURL=mother-exclamation-mark.js.map
